6a1c78919359205c0b0f09ac2f6d8e8c474ef14a
Kanishk Kundu <kanishk.kundu@nyu.edu>
2026-03-13 14:19:22 -0400

    Remove age onboarding background art and scrolling

--- app/src/main/java/com/bepresent/android/ui/onboarding/v2/screens/QuestionScreen.kt
+++ app/src/main/java/com/bepresent/android/ui/onboarding/v2/screens/QuestionScreen.kt
@@ -1,13 +1,11 @@
 package com.bepresent.android.ui.onboarding.v2.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -23,10 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.bepresent.android.R
 import com.bepresent.android.ui.onboarding.v2.OnboardingTokens
 import com.bepresent.android.ui.onboarding.v2.OnboardingTypography
 import com.bepresent.android.ui.onboarding.v2.QuestionOption
@@ -85,22 +81,18 @@ fun QuestionScreen(
                 .weight(1f)
                 .fillMaxWidth()
         ) {
-            if (isAgeQuestion) {
-                Image(
-                    painter = painterResource(R.drawable.session_brick),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter)
-                        .size(220.dp)
-                        .offset(y = 28.dp)
-                )
-            }
-
             // Options list
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
+                    .then(
+                        if (isAgeQuestion) {
+                            Modifier.fillMaxWidth()
+                        } else {
+                            Modifier
+                                .fillMaxSize()
+                                .verticalScroll(rememberScrollState())
+                        }
+                    )
             ) {
                 // Adaptive spacing based on option count
                 val topSpacing = when {
@@ -123,7 +115,7 @@ fun QuestionScreen(
                     Spacer(modifier = Modifier.height(12.dp))
                 }
 
-                Spacer(modifier = Modifier.height(if (isAgeQuestion) 140.dp else 20.dp))
+                Spacer(modifier = Modifier.height(20.dp))
             }
         }
     }
